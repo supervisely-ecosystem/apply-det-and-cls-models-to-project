6db34324592d40a2aab5e74a1bc91d7029096590
Daniel Y <33942379+qanelph@users.noreply.github.com>
2022-06-30 16:35:14 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,8 +9,15 @@ import src.det_settings
 import src.connect_to_cls_model
 import src.cls_settings
 
+from supervisely.app.fastapi import available_after_shutdown
 
 @g.app.get("/")
+@available_after_shutdown(app=g.app)
 def read_index(request: Request):
     return g.templates_env.TemplateResponse('index.html', {'request': request})
 
+
+
+@g.app.on_event("shutdown")
+def shutdown():
+    read_index()  # save last version of static files
